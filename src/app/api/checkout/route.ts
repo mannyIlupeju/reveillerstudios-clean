2a7a74ea62872b90e05b81f02e4d19e5c4a4db3d
machnmb_ <mannybiz@outlook.com>
2025-07-11 00:07:14 -0400

    made changes to shopify_public

--- src/app/api/checkout/route.ts
+++ src/app/api/checkout/route.ts
@@ -21,7 +21,7 @@ export async function POST(req: Request) {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
-                "X-Shopify-Storefront-Access-Token": `${process.env.SHOPIFY_PUBLIC}`,
+                "X-Shopify-Storefront-Access-Token": `${process.env.NEXT_PUBLIC_SHOPIFY_PUBLIC}`,
             },
             body: JSON.stringify({
                 query,
