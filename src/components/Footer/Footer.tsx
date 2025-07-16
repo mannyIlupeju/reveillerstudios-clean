41b332ad497b1ac0676fe7ecfe10ab09d36b3abf
machnmb_ <mannybiz@outlook.com>
2025-07-15 20:10:59 -0400

    resolved path added zod

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,8 +3,8 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { useGlobalContext } from '@/Context/GlobalContext'
-import NewsletterFooter from '@/components/NewsletterFooter/NewsletterFooter'
+import { useGlobalContext } from '../../Context/GlobalContext';
+import NewsletterFooter from '../../components/Newsletter/NewsletterFooter'
 
 
 
