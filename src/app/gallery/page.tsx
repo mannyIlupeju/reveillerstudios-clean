2787f1d9fb6540f92e06a5d6a976dc55f738d02b
machnmb_ <mannybiz@outlook.com>
2025-07-28 14:32:44 -0400

    working on fixing errors in gallery page

--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,10 +1,22 @@
 import { listS3Objects } from "@/lib/s3Client";
 import FolderDisplay from './FolderDisplay';
 import React from 'react';
+import { useParams } from 'next/navigation';
 import './gallery.css';
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Gallery',
+  description: 'Explore our gallery of creative works and projects.',
+};
+
+type GalleryPageProps = {
+  params: Record<string, string>;
+  searchParams?: { prefix?: string };
+};
 
 
-export default async function GalleryPage({searchParams}: {searchParams: {prefix?: string}}) {
+export default async function GalleryPage({ searchParams }: { params: any; searchParams?: { prefix?: string } }) {
   const prefix = searchParams?.prefix || '';
   
   const { folders } = await listS3Objects(prefix);
