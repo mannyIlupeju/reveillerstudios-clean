34558837e57bc6984d9cfde4665a31951be64f15
machnmb_ <mannybiz@outlook.com>
2025-08-28 10:31:27 -0400

    added sitemaps

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    siteUrl: 'https://reveillerstudios.com', // replace with your real domain
+    generateRobotsTxt: true,           // also generates a robots.txt file
+    sitemapSize: 7000,                 // optional - split sitemaps if too big
+    changefreq: 'daily',
+    priority: 0.7,
+};
